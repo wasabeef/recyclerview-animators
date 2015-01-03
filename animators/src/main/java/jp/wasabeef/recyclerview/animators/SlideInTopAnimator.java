adc0bd85319e0a107dba53e44b9f6c1862e086f8
furiya-daichi <furiya_daichi@cyberagent.co.jp>
2015-01-04 00:12:19 +0900

    adjustment alpha param

--- animators/src/main/java/jp/wasabeef/recyclerview/animators/SlideInTopAnimator.java
+++ animators/src/main/java/jp/wasabeef/recyclerview/animators/SlideInTopAnimator.java
@@ -25,6 +25,7 @@ public class SlideInTopAnimator extends BaseItemAnimator {
     protected void animateRemoveImpl(final RecyclerView.ViewHolder holder) {
         ViewCompat.animate(holder.itemView)
                 .translationY(0)
+                .alpha(0)
                 .setDuration(getRemoveDuration())
                 .setListener(new DefaultRemoveVpaListener(holder))
                 .start();
@@ -34,12 +35,14 @@ public class SlideInTopAnimator extends BaseItemAnimator {
     @Override
     protected void preAnimateAdd(RecyclerView.ViewHolder holder) {
         ViewCompat.setTranslationY(holder.itemView, -holder.itemView.getHeight());
+        ViewCompat.setAlpha(holder.itemView, 0);
     }
 
     @Override
     protected void animateAddImpl(final RecyclerView.ViewHolder holder) {
         ViewCompat.animate(holder.itemView)
                 .translationY(0)
+                .alpha(1)
                 .setDuration(getAddDuration())
                 .setListener(new DefaultAddVpaListener(holder))
                 .start();
