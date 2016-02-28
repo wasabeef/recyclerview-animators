3029d52d7955d8491bafd6e45ffd3c42779ef523
Hiroki Oizumi <team.hiroq@gmail.com>
2016-02-28 17:25:14 +0900

    Added animation selection spinner into AdapterSampleActivity

--- example/src/main/java/jp/wasabeef/example/recyclerview/AdapterSampleActivity.java
+++ example/src/main/java/jp/wasabeef/example/recyclerview/AdapterSampleActivity.java
@@ -1,12 +1,24 @@
 package jp.wasabeef.example.recyclerview;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import jp.wasabeef.recyclerview.adapters.AnimationAdapter;
+import jp.wasabeef.recyclerview.adapters.SlideInBottomAnimationAdapter;
+import jp.wasabeef.recyclerview.adapters.SlideInLeftAnimationAdapter;
+import jp.wasabeef.recyclerview.adapters.SlideInRightAnimationAdapter;
 import jp.wasabeef.recyclerview.animators.FadeInAnimator;
 import jp.wasabeef.recyclerview.adapters.AlphaInAnimationAdapter;
 import jp.wasabeef.recyclerview.adapters.ScaleInAnimationAdapter;
@@ -15,6 +27,57 @@ import jp.wasabeef.recyclerview.adapters.ScaleInAnimationAdapter;
  * Created by Wasabeef on 2015/01/03.
  */
 public class AdapterSampleActivity extends AppCompatActivity {
+  private interface ConstantAdapter{
+      AnimationAdapter get(Context context);
+  }
+
+  enum Type {
+    AlphaIn(new ConstantAdapter() {
+      @Override
+      public AnimationAdapter get(Context context) {
+        MainAdapter adapter = new MainAdapter(context, new ArrayList<>(Arrays.asList(data)));
+        return new AlphaInAnimationAdapter(adapter);
+      }
+    }),
+    ScaleIn(new ConstantAdapter() {
+        @Override
+        public AnimationAdapter get(Context context) {
+            MainAdapter adapter = new MainAdapter(context, new ArrayList<>(Arrays.asList(data)));
+            return new ScaleInAnimationAdapter(adapter);
+        }
+    }),
+    SlideInBottom(new ConstantAdapter() {
+        @Override
+        public AnimationAdapter get(Context context) {
+            MainAdapter adapter = new MainAdapter(context, new ArrayList<>(Arrays.asList(data)));
+            return new SlideInBottomAnimationAdapter(adapter);
+        }
+    }),
+    SlideInLeft(new ConstantAdapter() {
+        @Override
+        public AnimationAdapter get(Context context) {
+            MainAdapter adapter = new MainAdapter(context, new ArrayList<>(Arrays.asList(data)));
+            return new SlideInLeftAnimationAdapter(adapter);
+        }
+    }),
+    SlideInRight(new ConstantAdapter() {
+        @Override
+        public AnimationAdapter get(Context context) {
+            MainAdapter adapter = new MainAdapter(context, new ArrayList<>(Arrays.asList(data)));
+            return new SlideInRightAnimationAdapter(adapter);
+        }
+    });
+
+    private ConstantAdapter mAdapter;
+
+    Type(ConstantAdapter adapter) {
+      mAdapter = adapter;
+    }
+
+    public AnimationAdapter getAdapter(Context context) {
+      return mAdapter.get(context);
+    }
+  }
 
   private static String[] data = new String[] {
       "Apple", "Ball", "Camera", "Day", "Egg", "Foo", "Google", "Hello", "Iron", "Japan", "Coke",
@@ -29,7 +92,11 @@ public class AdapterSampleActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_adapter_sample);
 
-    RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+    final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list);
 
     if (getIntent().getBooleanExtra("GRID", true)) {
       recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
@@ -37,12 +104,27 @@ public class AdapterSampleActivity extends AppCompatActivity {
       recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
+    Spinner spinner = (Spinner) findViewById(R.id.spinner);
+    ArrayAdapter<String> spinnerAdapter =
+        new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+    for (Type type : Type.values()) {
+      spinnerAdapter.add(type.name());
+    }
+    spinner.setAdapter(spinnerAdapter);
+    spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+      @Override
+      public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        recyclerView.setAdapter(Type.values()[position].getAdapter(AdapterSampleActivity.this));
+      }
+
+      @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+      }
+    });
+
     recyclerView.setItemAnimator(new FadeInAnimator());
     MainAdapter adapter = new MainAdapter(this, new ArrayList<>(Arrays.asList(data)));
     AlphaInAnimationAdapter alphaAdapter = new AlphaInAnimationAdapter(adapter);
-    ScaleInAnimationAdapter scaleAdapter = new ScaleInAnimationAdapter(alphaAdapter);
-    //        scaleAdapter.setFirstOnly(false);
-    //        scaleAdapter.setInterpolator(new OvershootInterpolator());
-    recyclerView.setAdapter(scaleAdapter);
+    recyclerView.setAdapter(alphaAdapter);
   }
 }
