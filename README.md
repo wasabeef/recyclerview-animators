de583bd37ee8fcab1b617e869e8c8aecb5a04b1b
wasabeef <dadadada.chop@gmail.com>
2017-03-17 13:30:55 -0700

    updated copyright

--- README.md
+++ README.md
@@ -275,7 +275,7 @@ Thanks
 License
 -------
 
-    Copyright 2015 Wasabeef
+    Copyright 2017 Wasabeef
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
--- animators/src/main/java/jp/wasabeef/recyclerview/animators/FlipInBottomXAnimator.java
+++ animators/src/main/java/jp/wasabeef/recyclerview/animators/FlipInBottomXAnimator.java
@@ -1,7 +1,7 @@
 package jp.wasabeef.recyclerview.animators;
 
 /**
- * Copyright (C) 2015 Wasabeef
+ * Copyright (C) 2017 Wasabeef
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
